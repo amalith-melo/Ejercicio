8faa7c9e1b468664dcbbcb74ce7c37d0d441cfa9
amalith-melo <amalithmeloestudios@gmail.com>
2025-08-15 13:47:05 -0500

    calcular promedio y decir si aprobo o no

--- Promedio.js
+++ Promedio.js
@@ -1,5 +1,5 @@
 
-  /*let analisisDelPromedio;
+  let analisisDelPromedio;
   const calificacion1aux = prompt("Ingresa la primera calificación");
     const calificacion2aux = prompt("Ingresa la segunda calificación");
     const calificacion3aux = prompt("Ingresa la tercera calificación");
@@ -29,30 +29,9 @@ analizarPromedio();
 const p = document.getElementById('resultadoComparacion');
 
 // Asignamos al atributo textContent de la etiqueta P, el resultado de la comparacion
-p.textContent = analisisDelPromedio;*/
-
-function analizarPromedio (nota1 , nota2, nota3) {
-    const promedio = (nota1 + nota2 + nota3) / 3;
-    return promedio >= 3.0 ? ('El promedio es ' + promedio  + ' ¡Felicidades, has aprobado!') : ('El promedio es ' + promedio +  ', lo siento, no has aprobado.');
-}
-
-const calificacion1aux = prompt("Ingresa la primera calificación");
-const calificacion2aux = prompt("Ingresa la segunda calificación");
-const calificacion3aux = prompt("Ingresa la tercera calificación");
-
-const primeraCalificacion = parseFloat(calificacion1aux);
-const segundaCalificacion = parseFloat(calificacion2aux);
-const terceraCalificacion = parseFloat(calificacion3aux);
+p.textContent = analisisDelPromedio;
 
 
-const analisisDelPromedio= analizarPromedio(primeraCalificacion , segundaCalificacion , terceraCalificacion);
-console.log(analisisDelPromedio);
-
-//Obtengo la etiqueta P que esta en el html por medio del id 'resultadoComparacion'
-const p = document.getElementById('resultadoComparacion');
-
-// Asignamos al atributo textContent de la etiqueta P, el resultado de la comparacion
-p.textContent = analisisDelPromedio;   
   
 
     
